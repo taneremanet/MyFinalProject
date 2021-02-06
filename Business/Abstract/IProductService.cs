f205a16b351e7bef2be30beb6fd8965f823a3dcb
operator <operator@DESKTOP-MC9O68R>
2021-02-06 23:55:12 +0300

    06.02.2021 Ders Sonu

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -1,4 +1,5 @@
 ﻿using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,8 +8,11 @@ namespace Business.Abstract
 {
     public interface IProductService
     {
+        // Product ile ilgili dis dunyaya neyi servis etmek istiyorsak
+
         List<Product> GetAll();
         List<Product> GetAllByCategoryId(int id);
         List<Product> GetByUnitPrice(decimal min, decimal max);
+        List<ProductDetailDto> GetProductDetails();
     }
 }
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
             //ProductManager productManager = new ProductManager(new InMemoryProductDal());
-            ProductManager productManager = new ProductManager(new EfProductDal());
+            //ProductManager productManager = new ProductManager(new EfProductDal());
 
             //foreach (var product in productManager.GetAll())
             //{
@@ -21,6 +21,36 @@ namespace ConsoleUI
             //{
             //    Console.WriteLine(product.ProductName);
             //}
+
+            //ProductTest();
+            //IoC
+            //CategoryTest();
+
+            // DTO - Data Transformation Object
+
+            // Iki tablodan join yaptik
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager.GetProductDetails())
+            {
+                Console.WriteLine(product.ProductName + "/" + product.CategoryName);
+            }
+
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
 
             foreach (var product in productManager.GetByUnitPrice(40, 100))
             {

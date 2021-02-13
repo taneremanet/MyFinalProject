cdaa77eb4a6841904b7ad208c31e43a794436dc0
operator <operator@DESKTOP-MC9O68R>
2021-02-13 22:47:10 +0300

    13.02.2021 Ders Sonu

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -1,5 +1,7 @@
 using Business.Abstract;
 using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -32,6 +34,7 @@ namespace WebAPI
             // IoC icin yazdik
             // Icinde data tutmuyorsak singleton uygun olur
             services.AddSingleton<IProductService, ProductManager>();
+            services.AddSingleton<IProductDal, EfProductDal>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
